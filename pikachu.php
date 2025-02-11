d20b9cf9141f5345612f0ee83e230481a87aaa95
LucasWMF <lucas.melofogac@gmail.com>
2025-02-11 14:53:34 -0300

    update v1.0

---
+++ pikachu.php
@@ -0,0 +1,47 @@
+<?php
+    include './header.php';    
+?>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-10 p-3">
+        <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img class="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPR7Pchz9YNZ7czmKUrhljwCPy58RIg4VFZQ&s" alt="First slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="https://i.pinimg.com/736x/d8/c9/af/d8c9afbc39eea6cb5b8ef75ca50a9228.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkJA8uG1t1YkecWX7b4oo1oxY37ovjMRom2g&s" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="https://epa-ds.vercel.app/etec/imgs/foto1.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="https://dscoteca.vercel.app/image/imagem5.jpeg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="https://dscoteca.vercel.app/image/imagem3.jpeg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="https://epa-ds.vercel.app/etec/imgs/foto7.jpg" alt="Third slide">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include './footer.php';    
+?>
